16a84e9d11e3bba776db1324eca366c267c191e6
Mohamad Jahani <m4m4lj@gmail.com>
2017-05-25 21:34:13 +0430

    refactor(background): refactor whole code

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-var MAX_DL_CHECK_TRY = 200;
-var DL_CHECK_DELAY = 50;
-
 // Enable page action on Github file pages
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, currentTab) {
   if (currentTab.status !== 'complete' || changeInfo.status !== 'complete') {
@@ -19,7 +16,6 @@ chrome.pageAction.onClicked.addListener(function(tab) {
   if (!isFileURL(url)) {
     return;
   }
-
   var rawURL = getRawURL(url);
   chrome.downloads.download({
     url: rawURL
@@ -27,6 +23,40 @@ chrome.pageAction.onClicked.addListener(function(tab) {
     checkDownload({id: id});
   });
 });
+
+// Check download and open file if it's downloaded
+function checkDownload(query, tries) {
+  tries = tries || 0;
+  chrome.downloads.search(query, function(dl) {
+    var currentDownload = dl[0];
+    var fileName;
+    var fileSize;
+    var downloadId;
+    chrome.downloads.onChanged.addListener(function downloadListener(download) {
+      // Set downloadId if the sizes are equal
+      if (download.fileSize && download.fileSize.current && download.fileSize.current == currentDownload.fileSize) {
+        fileSize = download.fileSize.current;
+        downloadId = download.id;
+        return;
+      }
+      // Set fileName
+      if (download.filename && download.filename.current) {
+        fileName = download.filename.current;
+        return;
+      }
+      // Ignore if it's not our download
+      if (download.id !== downloadId) {
+        return;
+      }
+      // If download is completed, we can open it in Code and return
+      if (download.state && download.state.current && download.state.current === 'complete') {
+        chrome.downloads.onChanged.removeListener(downloadListener);
+        openInCode(fileName);
+        return;
+      }
+    });
+  });
+}
 
 // Returns true if current url is a file url in Github
 function isFileURL(url) {
@@ -39,48 +69,16 @@ function getRawURL(url) {
   return url.replace('blob', 'raw');
 }
 
-// Check download and open file if it's downloaded
-function checkDownload(query, tries) {
-  tries = tries || 0;
-  chrome.downloads.search(query, function(dl) {
-    // If dl is started, set url and fileSize for next queries
-    // The ID will change after canceling download in your custom download manager and redownloading it in Chrome
-    // So, we'll use url and fileSize as our query fields
-    // We should also check file id to be equal or greater than our original id
-    if (dl.length && query.id && dl[0].id >= query.id) {
-      query = {
-        url: dl[0].url,
-        fileSize: dl[0].fileSize,
-        exists: true,
-        state: 'complete'
-      }
-    }
-    // If download is completed, we can open it in Code and return
-    if (dl.length && dl[0].state === 'complete') {
-      chrome.tabs.query({
-        currentWindow: true,
-        active: true
-      }, function(tabs) {
-        if (tabs.length === 0) {
-          return;
-        }
-        chrome.tabs.update(tabs[0].id, {
-          url: encodeURI('vscode://file' + dl[0].filename)
-        });
-      });
-
+function openInCode(file) {
+  chrome.tabs.query({
+    currentWindow: true,
+    active: true
+  }, function(tabs) {
+    if (!tabs.length) {
       return;
     }
-
-    // If tries are over and dl is not present yet it's enough
-    if (tries >= MAX_DL_CHECK_TRY && !dl.length) {
-      return;
-    }
-
-    // Increment retries
-    tries += 1;
-    setTimeout(function() {
-      checkDownload(query, tries);
-    }, DL_CHECK_DELAY);
+    chrome.tabs.update(tabs[0].id, {
+      url: encodeURI('vscode://file' + file)
+    });
   });
 }
